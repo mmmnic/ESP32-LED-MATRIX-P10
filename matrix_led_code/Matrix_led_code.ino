9dcdbee3598be30e9a5d4013ba87648db842c3fe
mmmnic <mmmnic1@gmail.com>
2021-04-01 00:08:32 +0700

    add note for interfaces

--- matrix_led_code/Matrix_led_code.ino
+++ matrix_led_code/Matrix_led_code.ino
@@ -1,5 +1,6 @@
 #include "Matrix_led_code.h"
 
+void draw
 void setup() {
   display.begin(4);
   display.setMuxPattern(BINARY);
@@ -20,3 +21,26 @@ void loop()
   display.drawPixel
   */
 }
+
+
+/**************************************************************************/
+/*!
+   void Adafruit_GFX::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,uint16_t color)
+   @brief    Draw a line
+    @param    x0  Start point x coordinate
+    @param    y0  Start point y coordinate
+    @param    x1  End point x coordinate
+    @param    y1  End point y coordinate
+    @param    color 16-bit 5-6-5 Color to draw with
+*/
+/**************************************************************************/
+
+/**************************************************************************/
+/*!
+   void GFXcanvas1::drawPixel(int16_t x, int16_t y, uint16_t color)
+   @brief    Draw a pixel to the canvas framebuffer
+    @param   x   x coordinate
+    @param   y   y coordinate
+    @param    color 16-bit 5-6-5 Color to fill with
+*/
+/**************************************************************************/
